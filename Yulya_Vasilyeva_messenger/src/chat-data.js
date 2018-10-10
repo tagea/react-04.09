45b6e5341c397fdbf7a000ffa1749f7f1a3bd0be
Yulya Vasilyeva <043@tut.by>
2018-10-10 20:25:47 +0300

    база для чатов

---
+++ Yulya_Vasilyeva_messenger/src/chat-data.js
@@ -0,0 +1,19 @@
+export const chatList = [
+    {
+        id: '1',
+        name: 'Разговоры о Frontend',
+        img: 'caht-1.jpg'
+    },
+    {
+        id: '2',
+        name: 'Разговоры о Design',
+        img: 'caht-2.jpg'
+    },
+    {
+        id: '3',
+        name: 'Поговорим о UX/UI',
+        img: 'caht-3.jpg'
+    }
+];
+
+
