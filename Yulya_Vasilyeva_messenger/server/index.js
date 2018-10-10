650fdcf775bee92caa784c72bb4c5cd513188d3a
Yulya Vasilyeva <043@tut.by>
2018-10-10 20:34:39 +0300

    настройка локального сервера

--- Yulya_Vasilyeva_messenger/server/index.js
+++ Yulya_Vasilyeva_messenger/server/index.js
@@ -1,10 +1,6 @@
-const express = require('express');
-const socketIO = require('socket.io');
-const http = require('http');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 server.listen(3000, () => {
   console.log('Server has been started');
@@ -12,13 +8,17 @@ server.listen(3000, () => {
 
 io.on('connection', (socket) => {
   // Записывать socket.id в базу
-  socket.on('message', (message) => {
-    socket.broadcast.emit('message', message);
-    socket.emit('message', message);
+  socket.on('user', (user) => {
+    socket.broadcast.emit('user', user);
+    socket.emit('user', user);
     // отправка адресно - socket.in(<id нужного сокета>).emit(...)
+  });
+  socket.on('comments', (comments) => {
+    socket.broadcast.emit('comments', comments);
+    socket.emit('comments', comments);
   });
 });
 
-io.on('disconnection', (socket) => {
-  // Удалить id из базы
-});
+// io.on('disconnection', (socket) => {
+//   // Удалить id из базы
+// });
