0477f7681cfe72de034543cc0964573aa996774a
Yulya Vasilyeva <043@tut.by>
2018-10-10 20:34:05 +0300

    Обработка данных через redux и вывод сообщений чата

--- Yulya_Vasilyeva_messenger/src/containers/MessageContainer.jsx
+++ Yulya_Vasilyeva_messenger/src/containers/MessageContainer.jsx
@@ -1,28 +1,75 @@
-//импорт React
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
 import Messages from 'components/MessageWindow/Messages';
+import SendForm from 'components/MessageWindow/SendForm';
+import { mountEventsGetComment, sendComment } from 'actions/comment';
 
-export default class MessagesContainer extends PureComponent {
-    constructor(props){
+class MessagesContainer extends PureComponent {
+    constructor(props) {
         super(props);
 
         this.state = {
-            comments: [],
-            isLoading: false,
-        }
+            author: '',
+            message: '',
+        };
+    }
+    //изменение формы с сообщением
+    changeInput = (ev) => {
+        this.setState({
+            [ev.target.name]: ev.target.value,
+        });
     }
 
-    componentDidMount() {
+    sendMessage = (event) => {
+        const { sendComment } = this.props;
+        const { message, author } = this.state;
+        
+        sendComment({message,author});
+        //после отправки, чистим поле для сообщения
         this.setState({
-            isLoading: true,
+            message: '',
+        });
+        event.preventDefault();
+    }
+    componentDidMount() {
+        const { match, mountEventsGetComment } = this.props;
+
+        mountEventsGetComment();
+        this.setState({
+            author: match.params.login,
         });
     }
     render() {
-        const { isLoading, comments } = this.state;
-        return(
-            <Messages isLoading={isLoading} comments={comments} />
+        const { message } = this.state;
+        const { comments } = this.props;
+        return (
+            <Fragment>
+                <Messages comments={comments} />
+                <SendForm message={message} changeInput={this.changeInput} sendMessage={this.sendMessage} />
+            </Fragment>
         );
-        
     }
 }
+MessagesContainer.propTypes = {
+    comments: PropTypes.arrayOf(PropTypes.object),
+    mountEventsGetComment: PropTypes.func,
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        ...ownProps,
+        comments: state.comment.commentList,
+    };
+}
+function mapDispatchToProps(dispatch, props) {
+    return {
+        ...props,
+        mountEventsGetComment: () => mountEventsGetComment(dispatch),
+        sendComment: sendComment(dispatch),
+    };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(withRouter(MessagesContainer));
