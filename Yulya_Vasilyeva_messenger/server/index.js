7990a41802fc1100698a57ce48df54348a1abf6a
Yulya Vasilyeva <043@tut.by>
2018-10-13 23:14:51 +0300

    настройка работы с MongoBD, удаленная БД

--- Yulya_Vasilyeva_messenger/server/index.js
+++ Yulya_Vasilyeva_messenger/server/index.js
@@ -1,8 +1,17 @@
-const app = require('express')();
+const app = require('express')();//приложение express
+const bodyParser = require('body-parser');//для обработки данных
+const mongoose = require('mongoose');//для работы с mongoDB
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server);//для работы с сокетами
 
-server.listen(3000, () => {
+//подключаем приложение к bodyparser
+app.use(bodyParser.json());
+//подключаемся к базе mongo
+mongoose.connect('mongodb+srv://user-super:VSUcD3WtCuKOO51d@cluster0-izll9.mongodb.net/messenger');
+//подлкючаем модели
+const { User } = require('./models');
+
+server.listen(3000, () => {//порт для работы сервера
   console.log('Server has been started');
 });
 
@@ -17,6 +26,24 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('comments', comments);
     socket.emit('comments', comments);
   });
+});
+
+app.get('/users', async(req, res) => {
+  //вывод, используя модель mongoose
+  const users = await User.find();
+  res.json(users);
+});
+
+app.get('/users/:id', async(req, res) => {
+  const user = await User.findById(req.params.id);//поиск по ID
+  res.json(user);
+});
+//отправляем пользователя в БД
+app.post('/users', async(req, res) => {
+  let user = new User(req.body);//в req.body содержится запрос в json
+  user = await user.save();//сохраняем в базу данные
+
+  res.json(user);
 });
 
 // io.on('disconnection', (socket) => {
--- Yulya_Vasilyeva_messenger/src/store.js
+++ Yulya_Vasilyeva_messenger/src/store.js
@@ -1,7 +1,27 @@
 //создаем хранлище данных
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 //импортируем корневой редьюсер
 import rootReducer from 'reducers/';
+
+//MIDDLEWARE
+//store - данные из стора
+//next - следующая по цепочке действие (reducer или middleware),
+//если не вызовем, то управление след. обработчику не передастся
+//можно проверять авторизацию
+//action - 
+/*const logger = store => next => action => {//длинный вариант без redux-logger
+    console.log('Before state', store.getState());
+    console.log('Action', action);
+    
+    const result = next(action);
+
+    console.log('Next state', store.getState());
+    return result;
+};*/
 //передаем в хранилище корневой редьюсер
-export default createStore(rootReducer);
+export default createStore(
+    rootReducer,
+    //process.env.mode === 'dev' для продакш исключение
+    applyMiddleware(logger),//применяем логгер
+);
